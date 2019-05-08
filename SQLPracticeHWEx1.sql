a4025a601d5884830f68195487b325c32000ee5c
Tahsin Khan <tahsin_k@live.co.uk>
2019-05-08 10:25:29 +0100

    completed Ex1 (except qs 1.6) and Ex2

---
+++ SQLPracticeHWEx1.sql
@@ -0,0 +1,35 @@
+--SQL Practice Sheet
+--Excersise 1
+
+USE Northwind
+--1.1
+--SELECT CustomerID, CompanyName, [Address] FROM Customers WHERE City = 'Paris' OR City = 'London';
+
+--1.2
+--SELECT ProductName FROM Products WHERE QuantityPerUnit LIKE '%bottles%';
+
+--1.3
+--SELECT Products.*, Suppliers.CompanyName, Suppliers.Country FROM Products INNER JOIN Suppliers 
+--ON Products.SupplierID = Suppliers.SupplierID WHERE QuantityPerUnit LIKE '%bottles%';
+
+--1.4
+--SELECT Products.CategoryID, CategoryName, COUNT(ProductName) AS 'PerCategoryCount'
+--FROM Products INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID 
+--GROUP BY Products.CategoryID, CategoryName ORDER BY [PerCategoryCount] DESC;
+
+--1.5
+--SELECT FirstName +' '+ LastName AS 'Employee Name', City AS 'City of Residence' FROM Employees;
+
+--1.6 List Sales Totals for all Sales Regions (via the Territories table using 4 joins) 
+-- with a Sales Total greater than 1,000,000. Use rounding or FORMAT to present the numbers.
+--1.6 IS INCOMPLETE
+--SELECT Territories.TerritoryID FROM Territories INNER JOIN EmployeeTerritories ON Territories.TerritoryID = EmployeeTerritories.TerritoryID 
+--WHERE EXISTS (SELECT EmployeeTerritories.EmployeeID FROM EmployeeTerritories INNER JOIN Orders ON EmployeeTerritories.EmployeeID = Orders.OrderID 
+--WHERE EXISTS (SELECT [Order Details].OrderID, SUM(UnitPrice*Quantity - (UnitPrice*Quantity*Discount)) AS 'Total' FROM [Order Details] INNER JOIN Orders ON [Order Details].OrderID = Orders.OrderID GROUP BY [Order Details].OrderID));
+
+--1.7
+--SELECT COUNT(OrderID) AS 'Orders With Freight > 100.00 in USA or UK' FROM Orders 
+--WHERE Freight > 100.00 AND ShipCountry = 'USA' OR ShipCountry = 'UK'; 
+
+--1.8
+--SELECT TOP 1 OrderID AS 'OrderID of the order with the highest discount applied', Discount FROM [Order Details] ORDER BY Discount DESC;
---
+++ SQLPracticeHWEx2.sql
@@ -0,0 +1,17 @@
+--SQL Practice sheet
+--Exersise 2
+--CREATE DATABASE SpartaGlobal;
+USE SpartaGlobal;
+
+/*CREATE TABLE SpartansTable(
+Title VARCHAR(10),
+FirstName VARCHAR(30),
+LastName VARCHAR(30), 
+University VARCHAR(50),
+Course VARCHAR(50),
+Grade VARCHAR(10));*/
+
+--INSERT INTO SpartansTable(Title, FirstName, LastName, University, Course, Grade) 
+--VALUES('Mr','Tahsin','Alam Khan','Univeristy of Greenwich','Mathematics BSc','2.1');
+
+--SELECT * FROM SpartansTable;
